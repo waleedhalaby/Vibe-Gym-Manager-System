e28c62b068f11cad208ff86700674a2dabb09bb5
w.medhat <waleed.halaby@outlook.com>
2018-07-11 23:04:49 +0200

    Add test project and project structure

--- EGITSGymManager/MainWindow.xaml.cs
+++ EGITSGymManager/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DomainModel.Helpers;
 using EGITSGymManager.Forms;
 using EGITSGymManager.Modules;
 
---
+++ Repos/EmployeeRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel.RepoInterfaces;
+
+namespace Repos
+{
+    public class EmployeeRepo : IEmployeeRepo
+    {
+        readonly string _connectionString;
+
+        public EmployeeRepo(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+    }
+}
